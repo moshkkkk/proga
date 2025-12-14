1b040fea862253c82b57087accd47542ef829cca
moshkkkk <roosasa461@gmail.com>
2025-12-14 17:53:33 +0700

    Обновим sort.c

--- lab9_git/sort.c
+++ lab9_git/sort.c
@@ -1,11 +1,25 @@
+#include <stdio.h>
+
 void bubble_sort(int arr[], int n){
-for (int i = 0; i < n-1; i++) 
-for (int j = 0; j < n-i-1; j++)
-if (arr[j] > arr[j+1]){
-int temp = arr[j];
-arr[j] = arr[j+1];
-arr[j+1] = temp;
+	for (int i = 0; i < n-1; i++){ 
+		for (int j = 0; j < n-i-1; j++){
+			if (arr[j] > arr[j+1]){
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
 }
+
+int main(){
+	int a[] = {4,2};
+	int n = sizeof(a) / sizeof(a[o]);
+	printf("Исходный массив: ");
+	for (int i=0; i<n; i++)
+		printf("%d", a[i]);
+	printf("\n");
+
+	return 0;
 }
-//First modification
-// Second modification
+
